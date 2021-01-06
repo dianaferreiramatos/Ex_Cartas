d8dc288cc866fd906c3d3df44c35e2324116558a
dianaferreiramatos <d.catarina.c.f@upskill.pt>
2021-01-06 15:12:33 +0000

    Carta + Baralho

--- src/main/java/Baralho.java
+++ src/main/java/Baralho.java
@@ -1,51 +1,104 @@
+
 import java.util.ArrayList;
-import java.util.Objects;
+import java.util.Collections;
+import java.util.List;
 
 public class Baralho {
 
-    ArrayList<Carta> cartasBaralho = new ArrayList<>();
-    TipoNaipe naipeBaralho;
 
-    public Baralho(ArrayList<Carta> cartasBaralho, TipoNaipe naipeBaralho) {
-        this.cartasBaralho = cartasBaralho;
-        this.naipeBaralho = naipeBaralho;
+    private ArrayList<Carta> baralho;
+
+    public static enum NumCartasBaralho {
+        BAR_40,
+        BAR_52
+    };
+
+    private static final NumCartasBaralho BARALHO_POR_OMISSAO = NumCartasBaralho.BAR_40;
+
+    // Constrói uma instância de {@code Baralho} recebendo o tipo de baralho.
+    public Baralho(NumCartasBaralho tipoDeBaralho) {
+        this.baralho = (tipoDeBaralho == NumCartasBaralho.BAR_40 ? new ArrayList<Carta>(40) :
+                                                                            new ArrayList<Carta>(52));
+        for (Carta.Naipe n : Carta.Naipe.values()) {
+            for (Carta.Tipo t : Carta.Tipo.values()) {
+                if (tipoDeBaralho == NumCartasBaralho.BAR_40
+                        && t.compareTo(Carta.Tipo.SENA) > 0
+                        && t.compareTo(Carta.Tipo.DAMA) < 0) {
+                    continue;
+                }
+                this.baralho.add(new Carta(t, n));
+            }
+        }
     }
 
-    public ArrayList<Carta> getCartasBaralho() {
-        return cartasBaralho;
+    //Constrói uma instância de {@code Baralho} com o baralho por omissão.
+    public Baralho() {
+        this(Baralho.BARALHO_POR_OMISSAO);
     }
 
-    public void setCartasBaralho(ArrayList<Carta> cartasBaralho) {
-        this.cartasBaralho = cartasBaralho;
-    }
-
-    public TipoNaipe getNaipeBaralho() {
-        return naipeBaralho;
-    }
-
-    public void setNaipeBaralho(TipoNaipe naipeBaralho) {
-        this.naipeBaralho = naipeBaralho;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Baralho baralho = (Baralho) o;
-        return Objects.equals(cartasBaralho, baralho.cartasBaralho) &&
-                naipeBaralho == baralho.naipeBaralho;
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(cartasBaralho, naipeBaralho);
+    //construtor copia
+        public Baralho(Baralho outroBaralho) {
+        this.baralho = new ArrayList<>(outroBaralho.baralho.size());
+        for (Carta c : outroBaralho.baralho) {
+            this.baralho.add(new Carta(c));
+        }
     }
 
     @Override
     public String toString() {
-        return "Baralho{" +
-                "cartasBaralho=" + cartasBaralho +
-                ", naipeBaralho=" + naipeBaralho +
-                '}';
+        StringBuilder s = new StringBuilder();
+        for (Carta c : this.baralho) {
+            s.append(c.toString());
+            s.append("\n");
+        }
+        return s.toString();
     }
+
+
+    public void baralhar() {
+        Collections.shuffle(this.baralho);
+    }
+
+
+    public void partir() {
+
+        // Reserva a primeira metade das cartas do baralho
+        List<Carta> mbar = new ArrayList<>(
+                this.baralho.subList(0, this.baralho.size() / 2));
+
+        // Remove a primeira metade das cartas do baralho
+        this.baralho.removeAll(mbar);
+
+        // Coloca no final a primeira metade das cartas do baralho
+        this.baralho.addAll(mbar);
+    }
+
+
+    public List<Carta> primeirasCartas(int n) {
+
+        // Reserva as primeiras n cartas do baralho
+        List<Carta> mbar = new ArrayList<>(
+                this.baralho.subList(0,
+                        (n < this.baralho.size() ? n : this.baralho.size())));
+
+        // Remove as primeiras n cartas do baralho
+        this.baralho.removeAll(mbar);
+
+        return mbar;
+    }
+
+    // Remove e devolve as últimas n cartas do baralho.
+    public List<Carta> ultimasCartas(int n) {
+        // Reserva as últimas n cartas do baralho
+        List<Carta> mbar = new ArrayList<>(
+                this.baralho.subList(
+                        (n > this.baralho.size() ? 0 : this.baralho.size() - n),
+                        this.baralho.size()));
+
+        // Remove as últimas n cartas do baralho
+        this.baralho.removeAll(mbar);
+
+        return mbar;
+    }
+
 }
--- src/main/java/Carta.java
+++ src/main/java/Carta.java
@@ -1,50 +1,88 @@
-import java.util.Objects;
+public class Carta implements Comparable<Carta> {
 
-public class Carta {
 
-    public static TipoCarta tipo;
-    public static TipoNaipe naipe;
+    public static enum Tipo {
+        DUQUE, TERNO, QUADRA, QUINA, SENA, OITO, NOVE, DEZ,
+        DAMA, VALETE, REI, MANILHA, AS
+    };
 
-    public Carta(TipoCarta tipo, TipoNaipe naipe) {
+    public static enum Naipe {
+        COPAS, OUROS, ESPADAS, PAUS
+    };
+
+
+    private Tipo tipo;
+    private Naipe naipe;
+
+    private static final Tipo DEFAULT_TIPO = Tipo.DUQUE;
+    private static final Naipe DEFAULT_NAIPE = Naipe.COPAS;
+
+    public Carta(Tipo tipo, Naipe naipe) {
         this.tipo = tipo;
         this.naipe = naipe;
     }
 
-    public TipoCarta getTipo() {
-        return tipo;
+     //ignorar este construtor, nao acrescenta valor neste exercicio
+     //public Carta(String descricao) {
+     //   String[] arrayDescricao = descricao.split(" ");
+     //   this.tipo = Tipo.valueOf(arrayDescricao[0]);
+     //  this.naipe = Naipe.valueOf(arrayDescricao[2]);
+     //}
+
+
+    public Carta() {
+        this(DEFAULT_TIPO, DEFAULT_NAIPE);
     }
 
-    public void setTipo(TipoCarta tipo) {
+    public Carta(Carta outraCarta) {
+        this(outraCarta.tipo, outraCarta.naipe);
+    }
+
+    public Tipo getTipo() {
+        return this.tipo;
+    }
+
+    public Naipe getNaipe() {
+        return this.naipe;
+    }
+
+    public void setTipo(Tipo tipo) {
         this.tipo = tipo;
+
     }
 
-    public TipoNaipe getNaipe() {
-        return naipe;
-    }
-
-    public void setNaipe(TipoNaipe naipe) {
+    public void setNaipe(Naipe naipe) {
         this.naipe = naipe;
-    }
 
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Carta carta = (Carta) o;
-        return tipo == carta.tipo &&
-                Objects.equals(naipe, carta.naipe);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(tipo, naipe);
     }
 
     @Override
     public String toString() {
-        return "Carta{" +
-                "tipo=" + tipo +
-                ", naipe=" + naipe +
-                '}';
+        return this.tipo.toString() + " de " + this.naipe.toString();
     }
+
+    @Override
+    public boolean equals(Object outroObjeto) {
+        if (this == outroObjeto) {
+            return true;
+        }
+        if (outroObjeto == null || this.getClass() != outroObjeto.getClass()) {
+            return false;
+        }
+        Carta outraCarta = (Carta) outroObjeto;
+        return this.tipo == outraCarta.tipo
+                && this.naipe == outraCarta.naipe;
+    }
+
+    @Override
+    public int compareTo(Carta outraCarta) {
+        int resultado = this.getNaipe().compareTo(outraCarta.getNaipe());
+
+        if (resultado == 0) {
+            resultado = this.getTipo().compareTo(outraCarta.getTipo());
+        }
+
+        return resultado;
+    }
+
 }
